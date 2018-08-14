cfe3fcf63e78f48dc2fd5189d24975f8cdd0a9f9
Vinicius Brandao <vinicius.brandao.araujo@ccc.ufcg.edu.br>
2018-08-13 22:52:30 -0300

    Grouping

---
+++ grouping/grouping-gamesCup/README.md
@@ -0,0 +1,1 @@
+# Analysis-Games-Cup
---
+++ grouping/grouping-movies/code/plota_solucoes_hclust.R
@@ -0,0 +1,65 @@
+get_grupos <- function(agrupamento, num_grupos){
+    agrupamento %>% 
+        cutree(num_grupos) %>% 
+        as.data.frame() %>% 
+        mutate(label = rownames(.)) %>% 
+        gather(key =  "k", value = "grupo", -label) %>% 
+        mutate(grupo = as.character(grupo))
+}
+
+plota_hclusts_1d = function(dados_filme, 
+                            nome_coluna, # coluna usada para distâncias
+                            dist_method = "euclidean", 
+                            linkage_method = "complete", 
+                            ks = 1:9,
+                            palette = "Set2"){
+    #' Retorna um ggplot das soluções de agrupamento de `dados_filme` 
+    #' para as quantidades de grupos em `ks` usando `hclust`.
+    library(ggplot2)
+    library(dplyr, warn.conflicts = F)
+    
+    agrupamento_h = dados_filme %>% 
+        column_to_rownames("filme") %>% 
+        select_(nome_coluna) %>%
+        dist(method = dist_method) %>% 
+        hclust(method = linkage_method)
+    
+    atribuicoes = get_grupos(agrupamento_h, num_grupos = ks)
+    
+    atribuicoes = atribuicoes %>% 
+        mutate(filme = label) %>% 
+        left_join(filmes, by = "filme")
+    
+    atribuicoes %>% 
+        ggplot(aes_string(y = nome_coluna, colour = "grupo")) + 
+        geom_jitter(aes(x = " "), 
+                    width = .02, height = 0, size = 1.6, alpha = .6) + 
+        facet_wrap(~ paste(k, " grupos")) + 
+        scale_color_brewer(palette = palette) + 
+        xlab("") 
+}
+
+plota_hclusts_2d = function(agrupamento,
+                            dados_filme,
+                            nome_colunas, # coluna usada para distâncias
+                            dist_method = "euclidean", 
+                            linkage_method = "complete", 
+                            ks = 1:9,
+                            palette = "Set2"){
+    #' Retorna um ggplot das soluções de agrupamento de `dados_filme` 
+    #' para as quantidades de grupos em `ks` usando `hclust`.
+    library(ggplot2)
+    library(dplyr, warn.conflicts = F)
+    
+    atribuicoes = get_grupos(agrupamento, num_grupos = ks)
+    
+    atribuicoes = atribuicoes %>% 
+        mutate(filme = label) %>% 
+        left_join(filmes, by = "filme")
+    
+    atribuicoes %>% 
+        ggplot(aes_string(x = nome_colunas[1], y = nome_colunas[2], colour = "grupo")) + 
+        geom_jitter(width = .02, height = 0, size = 1.6, alpha = .6) + 
+        facet_wrap(~ paste(k, " grupos")) + 
+        scale_color_brewer(palette = palette)
+}
